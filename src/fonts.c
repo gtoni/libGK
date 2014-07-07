9c1a7f2914436ec5de213d89161d7b2be09dafdc
Toni Georgiev <georgiev.toni@gmail.com>
2014-07-07 12:19:16 +0300

    Tweaked fonts a little.

--- src/fonts.c
+++ src/fonts.c
@@ -482,6 +482,7 @@ gkGlyphSet* gkGetGlyphSet(gkGlyphCollection* collection, int index){
 		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_FALSE);
 #ifdef GK_PLATFORM_WEB
 		free(tmp);
 #endif
